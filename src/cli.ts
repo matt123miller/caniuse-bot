cecd3fb26e57c77975a51651c5054e9aafc207f2
Matt Miller <matt123miller@hotmail.co.uk>
2020-10-17 14:57:14 +0100

    Some gloriously faked CLI input, but in theory I'm close

--- src/cli.ts
+++ src/cli.ts
@@ -4,22 +4,37 @@ export default function () {
   const feature = 'grid';
   const result = searchForFunctionality(feature);
 
-  if (Array.isArray(result)) {
-    // @ts-ignore
-    const userPrompt = formatMultipleMatches(result);
-    console.log(userPrompt);
 
-  }
-  else {
-    console.log(`feature ${feature} is supported like so: `, result);
+  console.log('Pretend the user just entered "ciu grid"');
+  // @ts-ignore
+  console.log(formatMultipleMatches(searchForFunctionality('grid')));
 
-  }
+  console.log('pretend the user just entered "1"');
+
+  // @ts-ignore
+  console.log();
+
+  console.log(`feature css-grid is supported like so: `,
+    searchForFunctionality('css-grid')
+);
+
+  
+  // if (Array.isArray(result)) {
+  //   // @ts-ignore
+  //   const userPrompt = formatMultipleMatches(result);
+  //   console.log(userPrompt);
+
+  // }
+  // else {
+  //   console.log(`feature ${feature} is supported like so: `, result);
+
+  // }
 }
 
 function formatMultipleMatches(matches: string[]): string {
   return [
     'Multiple matches found. Which one were you looking for?',
     'Type the number or "c" to cancel.',
-    ...matches.map((match, i) => `    [${i}] - ${match}`)
+    ...matches.map((match, i) => `    [${i+1}] - ${match}`)
   ].join('\n')
 }
