ff1ef9ed0ccebaafda15bf5c493f4bde323b587c
Matt Miller <matt123miller@hotmail.co.uk>
2020-10-17 14:39:52 +0100

    Pretending the cli file kind of works like CLI for now
    Moved console logging out of the core and into there.

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,25 @@
 import searchForFunctionality from './core/searchForFunctionality'
 
-export default async function() {
-  await searchForFunctionality('grid')
+export default function () {
+  const feature = 'grid';
+  const result = searchForFunctionality(feature);
+
+  if (Array.isArray(result)) {
+    // @ts-ignore
+    const userPrompt = formatMultipleMatches(result);
+    console.log(userPrompt);
+
+  }
+  else {
+    console.log(`feature ${feature} is supported like so: `, result);
+
+  }
+}
+
+function formatMultipleMatches(matches: string[]): string {
+  return [
+    'Multiple matches found. Which one were you looking for?',
+    'Type the number or "c" to cancel.',
+    ...matches.map((match, i) => `    [${i}] - ${match}`)
+  ].join('\n')
 }
